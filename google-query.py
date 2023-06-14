98e87a25eab79d8d37fbe6145dec4605bedda3ea
worldofchristian <christianwaters@protonmail.com>
2023-06-14 03:28:13 -0600

    first commit

---
+++ google-query.py
@@ -0,0 +1,52 @@
+import os
+import requests
+import json
+import pickle
+
+# Google Places API
+api_key = 'YOUR_API_KEY'
+
+with open("cities.pickle", "rb") as file:
+    imported_cities = pickle.load(file)
+
+# Function to get places for a specific city
+def get_places_for_city(city):
+    # Search query
+    query = f'bars and restaurants in {city}'
+
+    # API request
+    url = f'https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&key={api_key}'
+    response = requests.get(url)
+
+    # API response
+    if response.status_code == 200:
+        data = response.json()
+
+        # Extract the names of places from the response
+        results = data.get('results', [])
+        place_names = [place['name'] for place in results]
+
+        return place_names
+
+    else:
+        print(f'Error occurred while searching for {city}. Status code: {response.status_code}')
+        return []
+
+# Create a folder for city-place if it doesn't exist
+if not os.path.exists("city-place"):
+    os.makedirs("city-place")
+
+# Iterate over each city
+for city in imported_cities:
+    # Get places for the city
+    places = get_places_for_city(city)
+
+    # Create a dictionary for the city
+    city_data = {"city": city, "places": places}
+
+    # Create a file for the city and write the JSON data
+    filename = f"city-place/{city}.json"
+    with open(filename, "w") as file:
+        json.dump(city_data, file)
+
+print("Files created successfully in the 'city-place' folder.")
---
+++ gpt-query.py
@@ -0,0 +1,58 @@
+import os
+import json
+import openai
+
+openai.api_key = 'YOUR_API_KEY'
+
+# Read JSON files from the city-place directory
+directory = "city-place"
+file_list = os.listdir(directory)
+
+# Create the city-place-contact directory if it doesn't exist
+output_directory = "city-place-contact"
+os.makedirs(output_directory, exist_ok=True)
+
+# Iterate over each file
+for filename in file_list:
+    filepath = os.path.join(directory, filename)
+
+    # Read the JSON file
+    with open(filepath, "r") as file:
+        data = json.load(file)
+
+    # Extract the places from the JSON data
+    places = data.get("places", [])
+
+    # Create a chat conversation with the places as user inputs
+    chat = [
+        {
+            "role": "system",
+            "content": "I am doing lead generation and need your help.\n"
+                       "I will send you JSON containing names of bars and restaurants in a given city.\n"
+                       "You will generate new JSON containing the contact information of key decision makers at each establishment."
+        }
+    ]
+    for place in places:
+        chat.append({"role": "user", "content": place})
+
+    # Make the API prompt
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat,
+        max_tokens=100  # Adjust the max tokens limit as per your needs
+    )
+
+    # Get the assistant's reply
+    assistant_reply = response.choices[0].message['content']
+
+    # Create a new JSON file with the assistant's reply
+    output_filepath = os.path.join(output_directory, f"{filename.split('.')[0]}_contact.json")
+    output_data = {"response": assistant_reply}
+
+    with open(output_filepath, "w") as output_file:
+        json.dump(output_data, output_file, indent=4)
+
+    print("Prompt:", chat)
+    print("Assistant's Reply:", assistant_reply)
+    print(f"Generated file: {output_filepath}")
+    print("--------------------")
